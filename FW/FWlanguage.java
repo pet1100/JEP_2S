c215460c4e669e5488b188ede21d98943983f02e
Peter B. R. Jensen <pete5344@stud.kea.dk>
2015-05-10 12:26:20 +0200

    GUI 0.1

--- FW/FWlanguage.java
+++
@@ -1,9 +0,0 @@
-package FW;
-import DBE.DBAddress;
-
-public class FWlanguage extends DBAddress {
-
-	public Array<String> read() {
-		throw new UnsupportedOperationException();
-	}
-}
--- GUI/GUIMenu.java
+++
@@ -1,7 +0,0 @@
-package GUI;
-public class GUIMenu extends GuiMain {
-
-	public void makeButtons() {
-		throw new UnsupportedOperationException();
-	}
-}
