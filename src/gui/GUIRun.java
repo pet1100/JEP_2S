6ed650d34d5f557d81921f2b7187365a724cd571
Elinor Mohr <elinor.m.mikkelsen@gmail.com>
2015-05-27 13:54:41 +0200

    Look and feel sat på gui

--- src/gui/GUIRun.java
+++ src/gui/GUIRun.java
@@ -1,4 +1,7 @@
 package gui;
+//@Elinor
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 
 public class GUIRun
@@ -6,6 +9,19 @@ public class GUIRun
 	
 	public static void main(String[] args)
 	{
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) { //ser igennem listen af instaleret look and feel
+		        if ("Nimbus".equals(info.getName())) {// er look and feelet Nimbus?
+		            UIManager.setLookAndFeel(info.getClassName());// hvis ja, sæt til nimbus
+		        }
+		    }
+		} catch (Exception e) {
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());// går noget galt i loopet, sæt look and feel til samme som OS
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	    GUIMain gm = new GUIMain();
 	}
 
